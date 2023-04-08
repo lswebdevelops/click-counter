3a312c04e7693363e3232e1052370ad8d1dbdd0c
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-08 13:53:46 -0300

    buttons clic and reiniciar working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
-
 import './App.css';
+import Boton from './componentes/Boton'
 
 function App() {
+
+  const manejarClic = () =>{
+    console.log('Clic');
+  }
+const reiniciarContador =()=>{
+  console.log('Reiniciar');
+}
+
   return (
     <div className="App">
       <div className='frecodecamp-logo-contenedor'>
@@ -12,7 +20,14 @@ function App() {
         alt='Logo de freeCodeCamp' />
       </div>
       <div className='contenedor-principal'>
-        
+        <Boton
+        texto='Clic'
+        esBotonDeClic={true}
+        manejarClic={manejarClic} />
+        <Boton 
+        texto='Reiniciar'
+        esBotonDeClic={false}
+        manejarClic={reiniciarContador} />
       </div>
     </div>
   );
--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
-function Boton({ texto }){
+import '../hojas-de-estilo/Boton.css'
+// manejarClick es una función:
+function Boton({ texto, esBotonDeClic, manejarClic }){
   return(
-    <button>
+    <button 
+    // if the class is click, the class is boton-clic, otherwise boton-reiniciar(llamado boton ternário)
+    className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
+    onClick={manejarClic}>
       {texto}
     </button>
   )
 }
+export default Boton;
+
