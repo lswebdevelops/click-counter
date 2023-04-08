9468e8c5bfa90ec834c9750cbc46a9c203a9586f
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-08 14:22:05 -0300

    function contador added and working

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 import './App.css';
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
+import { useState } from 'react';
 
 function App() {
+// creating a hook: useState
+  const [numClics, setNumClics] = useState(0);
 
   const manejarClic = () =>{
-    console.log('Clic');
+  setNumClics(numClics + 1);
   }
 const reiniciarContador =()=>{
-  console.log('Reiniciar');
+  setNumClics(0)
 }
 
   return (
@@ -21,7 +24,7 @@ const reiniciarContador =()=>{
         alt='Logo de freeCodeCamp' />
       </div>
       <div className='contenedor-principal'>
-        <Contador numClics='5' />
+        <Contador numClics={numClics} />
         <Boton
         texto='Clic'
         esBotonDeClic={true}
