efae426b3d704d568266148181361b6bf2137336
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-12 21:46:17 -0300

    adding class component to Boton.jsx

--- src/componentes/Boton.jsx
+++ src/componentes/Boton.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import '../hojas-de-estilo/Boton.css'
 // manejarClick es una función:
-function Boton({ texto, esBotonDeClic, manejarClic }){
+
+class Boton extends React.Component{
+  render(){
   return(
     <button 
     // if the class is click, the class is boton-clic, otherwise boton-reiniciar(llamado boton ternário)
-    className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
-    onClick={manejarClic}>
-      {texto}
+    className={this.props.esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
+    onClick={this.props.manejarClic}>
+      {this.props.texto}
     </button>
   )
 }
+}
+
 export default Boton;
 
